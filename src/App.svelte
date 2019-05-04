aa0880ef02ea9bc5b46eefca15bb382a7e76a8d7
mehmet-erim <bilgi@mehmeterim.com>
2019-05-04 07:17:33 +0300

    Add modal and list component

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,19 @@
 <script>
-	const name = "Mehmet";
+		import List from "./components/List.svelte";
+
+		const name = "Mehmet";
 </script>
 
+<svelte:head>
+<link rel="stylesheet" href="https://bootswatch.com/4/materia/bootstrap.css">
+</svelte:head>
+
 <nav class="navbar navbar-light bg-light">
-  <a class="navbar-brand" href="#">
+  <a class="navbar-brand" href="/">
     <img src="https://svelte.dev/svelte-logo-horizontal.svg" width="150" height="50" alt="Svelte">
   </a>
 </nav>
 
-<svelte:head>
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-</svelte:head>
+<div class="container mt-5">
+  <List/>
+</div>
---
+++ src/components/List.svelte
@@ -0,0 +1,49 @@
+<script>
+  import Modal from "./Modal.svelte";
+
+  const customers = [
+    {
+      id: 1,
+      name: "Mark",
+      email: "Otto@markotto.com",
+      phone: "+231323232321"
+    }
+  ];
+
+  let showModal;
+</script>
+
+<style>
+  :global(.text-center) {
+    text-align: center;
+  }
+</style>
+
+<table class="table">
+	<thead class="thead-dark">
+		<tr>
+			<th scope="col">#</th>
+			<th scope="col">Name</th>
+			<th scope="col">Email</th>
+			<th scope="col">Phone</th>
+			<th scope="col" class="text-center">
+				<img src="https://img.icons8.com/color/26/000000/plus.png" alt="add-customer">
+			</th>
+		</tr>
+	</thead>
+	<tbody>
+    	{#each customers as {id, name, email, phone}}
+		<tr>
+			<th scope="row">{id}</th>
+			<td>{name}</td>
+			<td>{email}</td>
+			<td>{phone}</td>
+			<td class="text-center">
+            <button class="btn btn-sm btn-primary">Edit</button> 
+            <button class="ml-1 btn btn-sm btn-danger">Remove</button></td>
+		</tr>
+	{/each}
+	</tbody>
+</table>
+
+  <Modal bind:visible={showModal}/>
